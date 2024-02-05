a6a4b6fda6a8ee7379ee5bd7fde04c3aa5bc2733
regisatl <regisattolou21@outlook.fr>
2024-02-05 11:16:53 +0100

    ajout de la class Task

--- django_rest_api_tasks/tasks/models.py
+++ django_rest_api_tasks/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+      title = models.CharField(max_length=100) #title
+      description = models.TextField(blank=True, null=True) #description
+      completed = models.BooleanField(default=False) #completed
+      created_at = models.DateTimeField(auto_now_add=True) #created_at
+      
+      def __str__(self):
+            return self.title #return the task title
